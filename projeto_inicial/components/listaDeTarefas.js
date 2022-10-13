5bff6aec6ec0d64bb76cce08369308bf703537a4
ValterMonteiro <valtinms@gmail.com>
2022-10-13 16:03:08 -0400

    Adicionando Tarefa

--- projeto_inicial/components/listaDeTarefas.js
+++ projeto_inicial/components/listaDeTarefas.js
@@ -4,7 +4,18 @@ const criarTarefa = (evento)=>{
 
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
+    
+    /* percorrendo o dom e selecionando a lista */
+    const tarefa = document.querySelector('[data-task]');
+
+    /* capturando o valor digitado */
+    const conteudo = `<p class="content">${valor}</>`;
+
+    /* acessando e colocando a nova tarefa no app */
+    tarefa.innerHTML = conteudo;
+
+    /* limpando o input */
+    input.value = " ";
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
